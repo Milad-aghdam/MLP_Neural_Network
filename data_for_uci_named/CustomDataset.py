a860dad64c044e8f65a2fd65652c4952dadc6f50
miladagdam <miladagdam@gmail.com>
2024-08-22 11:04:57 +0330

    adding data set and main.py

--- data_for_uci_named/CustomDataset.py
+++ data_for_uci_named/CustomDataset.py
@@ -1,3 +1,22 @@
-print(" hi ")
-
 import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+
+
+class CustomDataset(Dataset):
+
+    def __init__(self, dataset_path):
+        data = pd.read_csv(dataset_path)
+        self.x = data.drop('stabf', axis=1)
+        self.y = data['stabf']
+
+        self.y = self.y.replace('unstable', 0)
+        self.y = self.y.replace('stable', 1)
+        
+        self.x = self.x.values
+        self.y = self.y.values 
+
+        print(self.y.shape)
+        print(self.y)
+
+data = CustomDataset('./data_for_uci_named/Dataset/Data_for_UCI_named.csv')
