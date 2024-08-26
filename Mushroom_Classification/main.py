38c3d7683f33c7ef84df5ab6edd4fcdafa904d31
miladagdam <miladagdam@gmail.com>
2024-08-26 10:49:18 +0330

    # Set Hyperparameters

--- Mushroom_Classification/main.py
+++ Mushroom_Classification/main.py
@@ -1,2 +1,16 @@
 from custom_dataset import CustomDataset
 from model import MLP
+
+import torch.optim
+import torch
+import torch.nn
+from torch.utils.data import DataLoader, random_split
+
+# Set Hyperparameters
+input_size = 18
+hidden_size = 64
+output_size = 2
+learning_rate = 0.01
+batch_size = 256
+num_epochs = 20
+
